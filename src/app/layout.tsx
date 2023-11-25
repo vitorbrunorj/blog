4f51bbce69f62b891e20076aec43e2016c9801ae
Vitor Bruno <vitorbrunorj@gmail.com>
2023-11-25 14:54:32 -0300

    Base projeto

---
+++ src/app/layout.tsx
@@ -0,0 +1,58 @@
+import { Analytics } from '@vercel/analytics/react'
+import type { Metadata } from 'next'
+import { Inter as FontSans } from 'next/font/google'
+import localFont from 'next/font/local'
+import { cn } from '@/lib/utils'
+
+import { ThemeProvider } from './_components/theme-provider'
+
+import './globals.css'
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans'
+})
+
+const fontHeading = localFont({
+  src: '../assets/fonts/CalSans-SemiBold.woff2',
+  variable: '--font-heading'
+})
+
+export const metadata: Metadata = {
+  title: 'The Next Dev - Full-stack Next.js 14',
+  description: 'Curso Full-stack Next.js 14',
+  keywords: [
+    'Next.js',
+    'Next.js 14',
+    'React.js',
+    'Lucas Nhimi',
+    'Curso Next.js',
+    'JavaScript',
+    'TypeScript',
+    'Full-stack',
+    'Tailwind CSS'
+  ]
+}
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable,
+          fontHeading.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+          <Analytics />
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
